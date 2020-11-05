069f22c7c5fe9618411106ed4df3d97080f9a031
Martin Acosta <javosmar@gmail.com>
2020-11-04 22:58:19 -0300

    listado de criptos con datos completo

--- src/routes/coins.routes.js
+++ src/routes/coins.routes.js
@@ -5,24 +5,14 @@
 const { Router } = require('express');
 const router = Router();
 const passport = require('passport');
-const https = require('https');
 const axios = require('axios');
 
-let promisesArray = [];
-
+/**
+ * Obtiene el listado de criptomonedas con sus datos desde la API de Coingecko
+ */
 async function getCryptos() {
     try {
-        const response = await axios.get('https://api.coingecko.com/api/v3/coins/list');
-        return response.data;
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-async function getInfo(id) {
-    try {
-        const response = await axios.get('https://api.coingecko.com/api/v3/coins/' + id);
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins');
         return response.data;
     }
     catch (error) {
@@ -31,29 +21,23 @@ async function getInfo(id) {
 }
 
 /**
- * Ruta que devuelve datos no protegidos
+ * Ruta que devuelve el listado de todas las criptomonedas disponibles
+ * @returns [{symbol, current_price, name, image, last_updated}]
  */
 router.get('/list-all', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    // const cryptos = ['01coin', 'arion', 'ark'];
-    let cryptosInfo = [];
-
+    const moneda = 'ars';
+    let cryptosList = [];
     const cryptos = await getCryptos();
-    console.log(cryptos[0].id)
-    res.json(cryptos);
-    /* for (let i = 0; i < cryptos.length; i++) {
-        cryptosInfo.push(await getInfo(cryptos[i].id));
+    for(let i = 0; i < cryptos.length; i++){
+        cryptosList.push({
+            symbol: cryptos[i].symbol,
+            current_price: cryptos[i].market_data.current_price[moneda],
+            name: cryptos[i].name,
+            image: cryptos[i].image,
+            last_updated: cryptos[i].last_updated
+        })
     }
-    res.json(cryptosInfo) */;
-
-    /* Promise.all([getCryptos(), getInfo('01coin')])
-    .then(function(results) {
-        const cryptosArray = results[0].data;
-        const infoArray = results[1].data;
-        console.log(cryptosArray);
-        res.json(infoArray);
-    }); */
-
-    
+    res.json(cryptosList);
 });
 
 /**
